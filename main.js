bb27672e24c75fcde3143451e8846e3f91b69b9d
HRicardoMorales <ricardo.morales.et28@gmail.com>
2024-11-21 23:23:57 -0300

    2daPreEntrega

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-$('.loop').owlCarousel({
-    center: true,
-    items:2,
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
+    responsiveClass:true,
     responsive:{
-        600:{
-            items:4
+        320:{
+            items:1,
+            nav:true
+        },
+        550:{
+            items:2,
+            nav:true,
+            loop:true
+        },
+        800:{
+            items:3,
+            nav:true,
+            loop:true
         }
     }
-});
+})
